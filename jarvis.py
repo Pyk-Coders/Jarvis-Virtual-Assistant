f25d7521b2ded059fdea06d38d0975ae42d41673
krishna jain <69294021+krishnajain@users.noreply.github.com>
2021-05-31 08:28:54 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,573 @@
+import pyttsx3
+import webbrowser
+import smtplib
+import random
+import speech_recognition as sr
+import wikipedia
+import datetime
+import os
+import sys
+import time
+import calendar
+import subprocess
+import tkinter as tink
+import requests, json
+from win32com.client import Dispatch
+from words import word_list
+###################################################################
+def NewsFromBBC(): 
+    main_url = " https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=4dbc17e007ab436fb66416009dfb59a8"
+    open_bbc_page = requests.get(main_url).json() 
+    article = open_bbc_page["articles"] 
+    results = []   
+    for ar in article: 
+        results.append(ar["title"]) 
+    for i in range(len(results)): 
+        print(i + 1, results[i])  
+###################################################################333
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Not a valid guess.")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Congrats, you guessed the word! You win!")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+
+
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
+
+
+def hangman():
+    word = get_word()
+    play(word)
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        word = get_word()
+        play(word)
+
+
+#if __name__ == "__main__":
+   # hangman()
+
+################################################################
+def display_intro():
+    title = "** A Simple Math Quiz **"
+    print("*" * len(title))
+    print(title)
+    print("*" * len(title))
+
+
+def display_menu():
+    menu_list = ["1. Addition", "2. Subtraction", "3. Multiplication", "4. Integer Division", "5. Exit"]
+    print(menu_list[0])
+    print(menu_list[1])
+    print(menu_list[2])
+    print(menu_list[3])
+    print(menu_list[4])
+
+
+def display_separator():
+    print("-" * 24)
+
+
+def get_user_input():
+    user_input = int(input("Enter your choice: "))
+    while user_input > 5 or user_input <= 0:
+        print("Invalid menu option.")
+        user_input = int(input("Please try again: "))
+    else:
+        return user_input
+
+
+def get_user_solution(problem):
+    print("Enter your answer")
+    print(problem, end="")
+    result = int(input(" = "))
+    return result
+
+
+def check_solution(user_solution, solution, count):
+    if user_solution == solution:
+        count = count + 1
+        print("Correct.")
+        return count
+    else:
+        print("Incorrect.")
+        return count
+
+
+def menu_option(index, count):
+    number_one = random.randrange(1, 21)
+    number_two = random.randrange(1, 21)
+    if index == 1:
+        problem = str(number_one) + " + " + str(number_two)
+        solution = number_one + number_two
+        user_solution = get_user_solution(problem)
+        count = check_solution(user_solution, solution, count)
+        return count
+    elif index == 2:
+        problem = str(number_one) + " - " + str(number_two)
+        solution = number_one - number_two
+        user_solution = get_user_solution(problem)
+        count = check_solution(user_solution, solution, count)
+        return count
+    elif index == 3:
+        problem = str(number_one) + " * " + str(number_two)
+        solution = number_one * number_two
+        user_solution = get_user_solution(problem)
+        count = check_solution(user_solution, solution, count)
+        return count
+    else:
+        problem = str(number_one) + " // " + str(number_two)
+        solution = number_one // number_two
+        user_solution = get_user_solution(problem)
+        count = check_solution(user_solution, solution, count)
+        return count
+
+
+def display_result(total, correct):
+    if total > 0:
+        result = correct / total
+        percentage = round((result * 100), 2)
+    if total == 0:
+        percentage = 0
+    print("You answered", total, "questions with", correct, "correct.")
+    print("Your score is ", percentage, "%. Thank you.", sep = "")
+
+
+def main():
+    display_intro()
+    display_menu()
+    display_separator()
+
+    option = get_user_input()
+    total = 0
+    correct = 0
+    while option != 5:
+        total = total + 1
+        correct = menu_option(option, correct)
+        option = get_user_input()
+
+    print("Exit the quiz.")
+    display_separator()
+    display_result(total, correct)
+########################################################################
+board = [' ' for x in range(10)]
+
+def insertLetter(letter, pos):
+    board[pos] = letter
+
+def spaceIsFree(pos):
+    return board[pos] == ' '
+
+def printBoard(board):
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    
+def isWinner(bo, le):
+    return (bo[7] == le and bo[8] == le and bo[9] == le) or (bo[4] == le and bo[5] == le and bo[6] == le) or(bo[1] == le and bo[2] == le and bo[3] == le) or(bo[1] == le and bo[4] == le and bo[7] == le) or(bo[2] == le and bo[5] == le and bo[8] == le) or(bo[3] == le and bo[6] == le and bo[9] == le) or(bo[1] == le and bo[5] == le and bo[9] == le) or(bo[3] == le and bo[5] == le and bo[7] == le)
+
+def playerMove():
+    run = True
+    while run:
+        move = input('Please select a position to place an \'X\' (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('Sorry, this space is occupied!')
+            else:
+                print('Please type a number within the range!')
+        except:
+            print('Please type a number!')
+            
+
+def compMove():
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    move = 0
+
+    for let in ['O', 'X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if isWinner(boardCopy, let):
+                move = i
+                return move
+
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1,3,7,9]:
+            cornersOpen.append(i)
+            
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+            
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        
+    return move
+
+def selectRandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0,ln)
+    return li[r]
+    
+
+def isBoardFull(board):
+    if board.count(' ') > 1:
+        return False
+    else:
+        return True
+
+def game():
+    print('Welcome to Tic Tac Toe!')
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(isWinner(board, 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print('Sorry, O\'s won this time!')
+            break
+
+        if not(isWinner(board, 'X')):
+            move = compMove()
+            if move == 0:
+                print('Tie Game!')
+            else:
+                insertLetter('O', move)
+                print('Computer placed an \'O\' in position', move , ':')
+                printBoard(board)
+        else:
+            print('X\'s won this time! Good Job!')
+            break
+
+    if isBoardFull(board):
+        print('Tie Game!')
+######################################################################################
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[len(voices)-1].id)
+
+def speak(audio):
+    print('Computer: ' + audio)
+    engine.say(audio)
+    engine.runAndWait()
+
+def greetMe():
+    currentH = int(datetime.datetime.now().hour)
+    if currentH >= 0 and currentH < 12:
+        speak('Good Morning!')
+
+    if currentH >= 12 and currentH < 18:
+        speak('Good Afternoon!')
+
+    if currentH >= 18 and currentH !=0:
+        speak('Good Evening!')
+
+greetMe()
+
+speak('Hello Sir, I am your digital assistant LARVIS the Lady Jarvis!')
+speak('How may I help you?')
+
+
+def myCommand():
+   
+    r = sr.Recognizer()                                                                                   
+    with sr.Microphone() as source:                                                                       
+        print("Listening...")
+        #r.pause_threshold =  1
+        audio = r.listen(source)
+    try:
+        query = r.recognize_google(audio, language='en-in')
+        print('User: ' + query + '\n')
+        
+    except sr.UnknownValueError:
+        speak('Sorry sir! I didn\'t get that! Try typing the command!')
+        query = str(input('Command: '))
+
+    return query
+
+if __name__ == '__main__':
+
+    while True:
+    
+        query = myCommand();
+        query = query.lower()
+        
+        if 'open youtube' in query:
+            speak('okay')
+            webbrowser.open('www.youtube.com')
+            
+        elif 'maths quiz' in query:
+            main()
+            
+        elif 'open calendar' in query:
+            yy = int(input("Enter year: "))
+            mm = int(input("Enter month: "))
+            print(calendar.month(yy, mm))
+
+        elif 'current weather' in query:
+            api_key = "858771e835bded9c1355767960d3b3c9"  
+            base_url = "http://api.openweathermap.org/data/2.5/weather?"   
+            city_name = input("Enter city name : ")   
+            complete_url = base_url + "appid=" + api_key + "&q=" + city_name   
+            response = requests.get(complete_url) 
+            x = response.json()
+            if x["cod"] != "404": 
+                y = x["main"] 
+                current_temperature = y["temp"]-273
+                current_pressure = y["pressure"] 
+                current_humidiy = y["humidity"] 
+                z = x["weather"] 
+                weather_description = z[0]["description"]
+                print(" Temperature (in celcius) = " +
+                    str(current_temperature) +
+                      "\n atmospheric pressure (in hPa unit) = " +
+                    str(current_pressure) +
+                      "\n humidity (in percentage) = " +
+                    str(current_humidiy) +
+                      "\n description = " +
+                    str(weather_description)) 
+            else: 
+               print(" City Not Found ") 
+
+        elif 'open google' in query:
+            speak('okay')
+            webbrowser.open('www.google.co.in')
+
+        elif 'open calculator' in query:
+            subprocess.Popen('C:\\Windows\\System32\\calc.exe')
+
+        elif 'open notepad' in query:
+            subprocess.Popen('C:\\Windows\\System32\\notepad.exe')
+            
+        elif 'current news' in query:
+            NewsFromBBC()
+            
+        elif 'stopwatch' in query:
+             s1 = input("press enter to start the timer")
+             begin = time.time()
+             endtimer = input("press enter to stop timer")
+             end = time.time()
+             elapsed = end - begin
+             print("time elapsed is",elapsed,"second")
+             
+        elif 'open reminder' in query:
+            import time
+            print("What shall I remind you about?")
+            text = str(input())
+            print("In how many minutes?")
+            local_time = float(input())
+            local_time = local_time * 60
+            time.sleep(local_time)
+            speak("reminder")
+            print(text)
+        elif 'tell me time' in query:
+            print(time.strftime("%H:%M:%S"))
+
+        elif 'tell me date' in query:
+            print (time.strftime("%d/%m/%Y"))
+            
+        elif 'open gmail' in query:
+            speak('okay')
+            webbrowser.open('www.gmail.com')
+            
+        elif 'play hangman' in query:
+            hangman()
+            
+        elif 'play tic tac toe' in query:
+            game()
+            
+        elif "what\'s up" in query or 'how are you' in query:
+            stMsgs = ['Just doing my thing!', 'I am fine!', 'Nice!', 'I am nice and full of energy']
+            speak(random.choice(stMsgs))
+
+        elif 'send email' in query:
+            speak('What should I say? ')
+            content = input("enter content :")
+            password = input("enter your paswword :")
+            resipient = input("enter email of recipient :")
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.ehlo()
+            server.starttls()
+            server.login("bunnyjoepy@gmail.com", password)
+            server.sendmail('bunnyjoepy@gmail.com', resipient, content)
+            server.close()
+            speak('Email sent!')
+
+        elif 'nothing' in query or 'abort' in query or 'stop' in query:
+            speak('okay')
+            speak('Bye Sir, have a good day.')
+            sys.exit()
+           
+        elif 'hello' in query:
+            speak('Hello Sir')
+
+        elif 'bye' in query:
+            speak('Bye Sir, have a good day.')
+            sys.exit()
+                                    
+        elif 'play music' in query:
+            music_folder = Your_music_folder_path
+            music = [music1, music2, music3, music4, music5]
+            random_music = music_folder + random.choice(music) + '.mp3'
+            os.system(random_music)
+                  
+            speak('Okay, here is your music! Enjoy!')
+            
+
+        else:
+            query = query
+            speak('Searching...')
+            try:
+                results = wikipedia.summary(query, sentences=5)
+                speak('Got it.')
+                speak('WIKIPEDIA says - ')
+                speak(results)
+                    
+            except:
+                webbrowser.open("https://google.com/search?q=%s" % query)
+        speak('Next Command! Sir!')
+        
